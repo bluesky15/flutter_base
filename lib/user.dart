0421cfbd540a0fb2e8e1271b4ff6a52bca87181f
lalit <lalit143.er@gmail.com>
2022-03-02 14:14:17 +0530

    tojson and fromjson added

--- lib/user.dart
+++ lib/user.dart
@@ -1,7 +1,9 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'user.freezed.dart';
+part 'user.g.dart';
 @freezed
 class User with _$User{
 const factory User({required String name, int? age,}) = _User;
+factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 }
